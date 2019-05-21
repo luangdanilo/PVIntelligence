c851470aa2315107f49e818fafd3ea42681243bd
luangdanilo <luang_danilo@yahoo.com.ph>
2019-05-21 14:13:04 +0800

    Added PRAC

--- PVAngular/src/app/app-routing.module.ts
+++ PVAngular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PharmacovigilanceComponent } from './pharmacovigilance/pharmacovigilance.component';
+import { PracComponent } from './prac/prac.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'pharmacovigilance', component: PharmacovigilanceComponent },
+  { path: 'prac', component: PracComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
