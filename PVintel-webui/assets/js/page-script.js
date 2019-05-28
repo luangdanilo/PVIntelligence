a360dd6e12f972acf780b2a134025c3ae8ee7b52
crsun <crsun@xtensa.com.ph>
2019-05-28 14:44:41 +0800

    nav active and map update

--- PVintel-webui/assets/js/page-script.js
+++ PVintel-webui/assets/js/page-script.js
@@ -1,6 +1,7 @@
 $(function () {
     loadbar();
-    setActive();
+	setActive();
+	Vmap();
 });
 
 
@@ -15,8 +16,38 @@ function setActive() {
 
     for (var i = 0; i < obj.length; i++) {
         if (document.location.href.indexOf(obj[i].href) >= 0) {
-            obj[i].className = "active";
+            obj[i].className = "active nav-link text-uppercase font-weight-bold";
         }
 
     }
+}
+
+
+function Vmap() {
+    jQuery(document).ready(function () {
+      jQuery('#vmap').vectorMap({
+        map: 'world_en',
+        backgroundColor: '#fff',
+        borderOpacity: 0.25,
+        borderWidth: 1,
+        color: '#ffffff',
+        enableZoom: true,
+        hoverOpacity: 0.7,
+        normalizeFunction: 'polynomial',
+        scaleColors: ['#C8EEFF', '#006491'],
+        values: sample_data,
+        selectedColor: '#666666',
+        selectedRegions: null,
+        showTooltip: true,
+        onRegionClick: function(element, code, region)
+        {
+            var message = 'You clicked "'
+                + region
+                + '" which has the code: '
+                + code.toUpperCase();
+
+            alert(message);
+        }
+      });
+    });
 }
